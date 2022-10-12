e3e6ad8b2c6c352f1a3aba85184028995fd60c76
Lag <56994201+Fgete@users.noreply.github.com>
2022-10-12 09:23:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Aquarium - </title>
+		<meta charset="utf-8"/>
+		<link rel="shortcut icon" type="image/x-icon" href="./images/logo.png"/>
+		<link rel="stylesheet" type="text/css" href="./css/colors.css">
+		<link rel="stylesheet" type="text/css" href="./css/font.css">
+		<link rel="stylesheet" type="text/css" href="./css/default.css">
+		<link rel="stylesheet" type="text/css" href="./css/global.css">
+	</head>
+
+	<body class="width_full height_full">
+		<header>
+			<div class="portrait"><div class="center bold">AQUARIUM</div></div>
+			<div class="landscape"><div class="center bold">AQUARIUM</div></div>
+		</header>
+
+		<div id="content" class="width_100 height_100">
+
+			<div class="width_100 height_100">
+
+				<div id="panel-bot" class="width_100 height_50">
+					<img src="./images/aquarium.png" class="width_100">
+
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<footer>
+			<div class="portrait"><div class="center bold">FOOTER PORTRAIT</div></div>
+			<div class="landscape"><div class="center bold">FOOTER LANDSCAPE</div></div>
+		</footer>
+	</body>
+
+</html>
